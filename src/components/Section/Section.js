7d9e5cb68fb004fca3f5ab752cf4fac1a331e450
Brittany Carusone <brcarusone@gmail.com>
2025-07-11 17:50:41 -0400

    initial commit

---
+++ src/components/Section/Section.js
@@ -0,0 +1,48 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import styles from './Section.module.css';
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Section({headline, scrollTo, goToSectionRef, showArrow, children}) {
+    const headlineRef = useRef();
+    const sectionRef = useRef();
+
+    useEffect(() => {
+        gsap.fromTo(
+            headlineRef.current,
+            {
+                autoAlpha: 0,
+                y: -20,
+            },
+            {
+                y: 0,
+                autoAlpha: 1,
+                duration: 1,
+                ease: "power3.out",
+                scrollTrigger: {
+                    scroller: ".container",
+                    trigger: headlineRef.current,
+                    start: "top 60%",
+                    end: "bottom 0%",
+                    toggleActions: "play none restart reverse",
+                }
+            },
+        );
+        return () => {};
+    }, []);
+
+    return (
+        <div className={styles.section} ref={sectionRef}>
+            <div>
+                <h2 ref={headlineRef}>{headline}</h2>
+            </div>
+            <div>{children}</div>
+            {showArrow && (
+                <button
+                    onClick={() => scrollTo(goToSectionRef)}
+                ></button>
+            )}
+        </div>
+    );
+}
